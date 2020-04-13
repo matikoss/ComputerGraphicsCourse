280be44e6501aacb74983bc76dea6b3f1da1dfdf
matikoss <m.kossakowski0@gmail.com>
2020-04-13 13:25:28 +0200

    Virtual Camera project refactor

--- VirtualCameraProj/src/main/java/main/Main.java
+++ VirtualCameraProj/src/main/java/main/Main.java
@@ -1,32 +1,15 @@
 package main;
 
 import core.Renderers.MainRenderer;
-import engine.graphics.Mesh;
-import engine.graphics.Renderer;
-import engine.graphics.Vertex;
 import engine.io.Input;
 import engine.io.Window;
-import engine.maths.Vector3f;
-import engine.objects.Camera;
 import org.lwjgl.glfw.GLFW;
 
 public class Main implements Runnable {
     public Thread game;
     public static Window window;
-    public Renderer renderer;
     public MainRenderer mainRenderer;
-    public Camera camera = new Camera(new Vector3f(0, 0, 0), new Vector3f(0, 0, 0));
     public final int WIDTH = 1280, HEIGHT = 720;
-
-    public Mesh mesh = new Mesh(new Vertex[]{
-            new Vertex(new Vector3f(-0.5f, 0.5f, 0.0f)),
-            new Vertex(new Vector3f(0.5f, 0.5f, 0.0f)),
-            new Vertex(new Vector3f(-0.5f, -0.5f, 0.0f)),
-            new Vertex(new Vector3f(-0.5f, -0.5f, 0.0f))
-    }, new int[]{
-            0, 1, 2,
-            0, 3, 2
-    });
 
 
     public void start() {
@@ -37,12 +20,9 @@ public class Main implements Runnable {
     public void init() {
         System.out.println("Init");
         window = new Window(WIDTH, HEIGHT, "Virtual Camera");
-        renderer = new Renderer();
         mainRenderer = new MainRenderer(WIDTH, HEIGHT);
         window.setBackgroundColor(0.0f, 0, 0);
-        //window.setFullscreen(true);
         window.create();
-        //mesh.create();
     }
 
     public void run() {
@@ -55,20 +35,11 @@ public class Main implements Runnable {
     }
 
     private void update() {
-        //System.out.println("Update");
         mainRenderer.mainController.update();
         window.update();
     }
 
     private void render() {
-        //System.out.println("Render");
-        //renderer.renderMesh(mesh);
-//        glBegin(GL_LINES);
-//        glVertex3f(0.0f, 0.0f, 0.0f);
-//        glVertex3f(0.1f, 0.0f, 0.0f);
-//        glVertex3f(0.0f, 0.0f, 0.0f);
-//        glVertex3f(0.0f, 0.1f, 0.0f);
-//        glEnd();
         mainRenderer.render();
         window.swapBuffers();
     }
